893b44d99bb28980a056d3d2c4a5302cbdacc1a8
Renato <renatosousafilho@gmail.com>
2020-05-08 15:51:41 -0300

    configurando redux e redux-saga

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './service/history';
+import { store, persistor } from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes></Routes>
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes></Routes>
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
