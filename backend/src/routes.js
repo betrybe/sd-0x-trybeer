90528a2db409a648447ce18602bc372883c2e45f
Renato <renatosousafilho@gmail.com>
2020-05-08 15:25:30 -0300

    configurando acesso para api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,4 +7,8 @@ import SessionController from './app/controllers/SessionController';
 routes.post('/register', UserController.store);
 routes.post('/login', SessionController.store);
 
+routes.get('/', (req, res) => {
+  return res.json({ ok: true });
+});
+
 export default routes;
